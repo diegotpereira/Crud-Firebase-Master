989c1aaada2a85518c31e7c5335b9ad1062490cb
Diego <diegoteixeirapereira@hotmail.com>
2021-11-05 12:21:02 -0300

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyB4WJ_nspMyFA_zsSZ9ky_SoQiS2QOp70I",
+    authDomain: "website-ecom-ff223.firebaseapp.com",
+    databaseURL: "https://website-ecom-ff223-default-rtdb.firebaseio.com",
+    projectId: "website-ecom-ff223",
+    storageBucket: "website-ecom-ff223.appspot.com",
+    messagingSenderId: "144186283636",
+    appId: "1:144186283636:web:165b58b28c7fcc3b9f6321",
+    measurementId: "G-JPMBDLNQDB"
+};
